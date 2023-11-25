b22f2c44ed12b090a360fadabe4c2ea8ac9c2d86
Dinara Mukhametzianova <muxametmdm@gmail.com>
2023-11-25 16:07:19 +0500

    добавила шаблоны

--- online_store/catalog/views.py
+++ online_store/catalog/views.py
@@ -3,3 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'catalog/index.html')
+def contacts(request):
+    if request.method == 'POST':
+        name=request.POST.get('name')
+        phone=request.POST.get('phone')
+        message=request.POST.get('message')
+    print(request.method)
+    return render(request, 'catalog/contacts.html')
