7f150a0f4822f26d06778e5513ea410e3b6de180
Dinara Mukhametzianova <muxametmdm@gmail.com>
2023-11-28 22:54:48 +0500

    модели и миграция 0001 (пункт 3)

--- online_store/catalog/models.py
+++ online_store/catalog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model  ):
+    name=models.CharField(max_length= 30)
+    message=models.TextField()
+    photo=models.ImageField(upload_to='static/')
+    name_category=models.CharField(max_length= 30)
+    unit_price=models.IntegerField()
+    date_of_creation=models.DateTimeField()
+    last_modified_date=models.DateTimeField()
+class Category(models.Model  ):
+    name_category=models.CharField(max_length= 30)
+    message_category=models.TextField()
+
--- online_store/online_store/settings.py
+++ online_store/online_store/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'online_store.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'online_shop',
+        'USER': 'postgres',
+        'PASSWORD': '12345',
+        'HOST': '127.0.0.1',
+        'PORT': 5432,
     }
 }
 
@@ -118,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_ROOt=os.path.join(BASE_DIR, 'static')
+#STATICFILES_ROOT=os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
@@ -127,3 +131,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# для mödel prodict
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
