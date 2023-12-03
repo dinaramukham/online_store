95053226f28403ac2939a418a6d835f46bc88ea1
Dinara Mukhametzianova <muxametmdm@gmail.com>
2023-12-03 22:32:06 +0500

    попытка добавить свой тег

--- online_store/catalog/templatetads/my_tags.py
+++
@@ -1,6 +0,0 @@
-from django import template
-register=template.library()
-#os.path.abspath(path)
-@register.simple_tag
-def mediapath(objects):
-    return
---
+++ online_store/catalog/templatetags/my_tags.py
@@ -0,0 +1,18 @@
+from django import template
+
+
+register=template.library()
+
+@register.simple_tag
+def mediapath():
+    return f'/static/imj/белая_чашка.png'
+
+
+
+#@register.filter
+#@register.simple_tag()
+#def mediapath(value):
+#    if value:
+#        return f'/static/{value}'
+#    else:
+#        return ''
